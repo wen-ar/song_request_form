0733a7e186e39b5ce96f180636d2beeb62dd7a40
wen-ar <wen-ar@outlook.com>
2026-02-02 01:06:51 +0800

    Change Microsoft OAuth URLs to use server metadata
    Updated Microsoft OAuth registration to use server metadata URL.

--- app.py
+++ app.py
@@ -30,8 +30,7 @@ microsoft = oauth.register(
     name="microsoft",
     client_id="d952e562-dd22-4a94-9dff-36593a201f31",   # Application (client) ID
     client_secret="AiK8Q~1zOKx3Dq5fm.pjhjCK6cQsPvgW1pWCcab0",                   # 需要在 Azure 建立
-    authorize_url="https://login.microsoftonline.com/00057328-0b9c-443f-ae16-0b2d1761430d/oauth2/v2.0/authorize",  # tenant ID
-    access_token_url="https://login.microsoftonline.com/00057328-0b9c-443f-ae16-0b2d1761430d/oauth2/v2.0/token",
+    servermetadataurl="https://login.microsoftonline.com/<tenant-id>/v2.0/.well-known/openid-configuration",
     client_kwargs={
         "scope": "openid email profile User.Read"
     }
